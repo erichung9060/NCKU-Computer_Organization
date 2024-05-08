baf98f95e85910f8f948b269207567764a2007a0
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2024-05-08 17:43:13 +0800

    Finished HW2

--- HW2/answer.h
+++ HW2/answer.h
@@ -1,3 +1,4 @@
+//----------------------------------3-1--------------------------------------
 #define macro_pi_cycle_count pi_cycle_count = \
     add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI + \
     lw_cnt * lw_CPI + sw_cnt * sw_CPI + others_cnt * others_CPI;
@@ -8,9 +9,7 @@
 #define macro_calc_pi_ratio pi_ratio = \
     (add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI \
     + others_cnt * others_CPI) / pi_cycle_count ;
-
-//---------------------------------------------------------------------------
-
+//----------------------------------3-2--------------------------------------
 #define macro_arraymul_baseline_cycle_count arraymul_baseline_cycle_count = \
     add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI + \
     lw_cnt * lw_CPI + sw_cnt * sw_CPI + others_cnt * others_CPI;
@@ -21,18 +20,14 @@
 #define macro_calc_arraymul_baseline_ratio arraymul_baseline_ratio = \
     (add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI \
     + others_cnt * others_CPI) / arraymul_baseline_cycle_count ;
-
-//---------------------------------------------------------------------------
-
+//----------------------------------3-3--------------------------------------
 #define macro_improved_version1_cycle_count improved_version1_cycle_count = \
     add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI + \
     lw_cnt * lw_CPI + sw_cnt * sw_CPI + others_cnt * others_CPI;
 
 #define macro_improved_version1_cpu_time improved_version1_cpu_time = \
     improved_version1_cycle_count * cycle_time;
-
-//----------------------------------------------------------------------------
-
+//----------------------------------3-4--------------------------------------
 #define macro_improved_version2_cycle_count improved_version2_cycle_count = \
     add_cnt * add_CPI + sub_cnt * sub_CPI + mul_cnt * mul_CPI + div_cnt * div_CPI + \
     lw_cnt * lw_CPI + sw_cnt * sw_CPI + others_cnt * others_CPI;
